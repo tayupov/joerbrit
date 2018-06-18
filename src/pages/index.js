4e1db11197edb2ebde7eabd3b80b0208a76887b9
tayupov <romantajupow@gmail.com>
2018-06-18 23:58:38 +0200

    spin

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,17 @@
 import React from "react"
+import styled from 'styled-components'
 
-export default () => <div>Hello world!</div>
+import './index.css'
+
+const Container = styled.div`
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    margin-left: -89px;
+    margin-top: -89px;
+`
+
+export default () =>
+    <Container>
+        <img className="image" src="https://i.imgur.com/sMPBu6M.png" alt="" />
+    </Container>
