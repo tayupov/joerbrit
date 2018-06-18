c1482183fb299a10d8c30904c78d60e698bccc7f
tayupov <romantajupow@gmail.com>
2018-06-19 00:03:41 +0200

    spin

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,15 +3,7 @@ import styled from 'styled-components'
 
 import './index.css'
 
-const Container = styled.div`
-    position: absolute;
-    top: 50%;
-    left: 50%;
-    margin-left: -89px;
-    margin-top: -89px;
-`
-
 export default () =>
-    <Container>
+    <div className="container">
         <img className="image" src="https://i.imgur.com/sMPBu6M.png" alt="" />
-    </Container>
+    </div>
