370678cd656a956e24a0c4b83d3cad400d8d1511
tayupov <romantajupow@gmail.com>
2018-06-19 01:25:46 +0200

    sound

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,30 @@
 import React from "react"
-import styled from 'styled-components'
 
 import './index.css'
 
-export default () =>
-    <div className="container">
-        <img className="image" src="https://i.imgur.com/sMPBu6M.png" alt="" />
-    </div>
+export default class Main extends React.Component {
+
+    constructor(props) {
+        super(props)
+        this.state = { running: false }
+    }
+
+    play = (e) => {
+        e.preventDefault()
+        const audio = document.getElementById("audio")        
+        this.state.running ? audio.pause() : audio.play()
+        this.setState({running: !this.state.running })
+    }
+
+    render() {
+        return (
+            <div className="container">
+                <img className="image" src="https://i.imgur.com/sMPBu6M.png" alt="" onClick={this.play} />
+                <audio
+                    id="audio"
+                    src="https://raw.githubusercontent.com/tayupov/joerbrit/master/src/pages/adore.mp3">
+                </audio>
+            </div>
+        )
+    }
+}
